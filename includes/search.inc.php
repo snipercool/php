931b355981b67b0759454adc87d612bc08cd1082
snipercool <sniper.cool@hotmail.com>
2019-05-02 11:24:43 +0200

    tried sql in database first before submitting here

--- includes/search.inc.php
+++ includes/search.inc.php
@@ -11,7 +11,9 @@ if (isset($_POST['submit'])) {
     if(preg_match("/^[A-Za-z]+/", $_POST['search'])){ 
         $search=$_POST['search'];
         $conn=Db::getInstance();
-        $stmt= $conn->prepare("");
+        $stmt= $conn->prepare("select id, fullname, username, Null as tag from user where fullname LIKE '%".$search."%' or username LIKE '%".$search."%'
+        UNION
+        select id, Null as fullname, Null as username, tag from tag where tag LIKE '%".$search."%'");
         $stmt->execute();
 
         
