16d22c00c997bb04bd450597aaf7e5a6fbb2f06c
snipercool <sniper.cool@hotmail.com>
2019-05-02 11:22:55 +0200

    connection and preparing sql

--- includes/search.inc.php
+++ includes/search.inc.php
@@ -6,44 +6,13 @@ require_once("../bootstrap.php");
 <input type="submit" name="submit" value="Search">
 </form>
 <?php
-    if (isset($_POST['submit'])) {
-        if(isset($_GET['search'])){
-        if(preg_match("/^[A-Za-z]+/", $_POST['search'])){ 
-            $search=$_POST['search'];
-            $conn=Db::getInstance();
-            $stmt= $conn->prepare("select id, fullname, username, Null as tag from user where fullname LIKE '%".$search."%' or username LIKE '%".$search."%'
-            UNION
-            select id, Null as fullname, Null as username, tag from tag where tag LIKE '%".$search."%'");
-            $stmt->execute();
+if (isset($_POST['submit'])) {
+    if(isset($_GET['search'])){
+    if(preg_match("/^[A-Za-z]+/", $_POST['search'])){ 
+        $search=$_POST['search'];
+        $conn=Db::getInstance();
+        $stmt= $conn->prepare("");
+        $stmt->execute();
 
-           while($row=$stmt->fetchAll(\PDO::FETCH_ASSOC)){ 
-               if ($row[0]['fullname'] != NULL) {
-                    $FullName=$row[0]['fullname']; 
-                    $UserName=$row[0]['username']; 
-                    $ID=$row[0]['id'];
-                    echo "<ul>\n"; 
-                    echo "<li>" . "<a  href=\"search.inc.php?username=$UserName\"><p>" .$UserName . "</p><p> " . $FullName .  "</p></a></li>\n"; 
-                    echo "</ul>"; 
-                    echo "<hr>";
-               }elseif ($row[0]['tag'] != NULL) {
-                    $tag=$row[0]['tag'];
-                    $ID=$row[0]['id'];
-                    echo "<ul>\n"; 
-                    echo "<li>" . "<a  href=\"search.inc.php?tag=$tag\"><p>" .$tag . "</p></a></li>\n"; 
-                    echo "</ul>"; 
-                    echo "<hr>";
-               }
-    	        
-                  
-                  
-            var_dump($row);
-            }
-        } 
-
-        }else {
-       echo "<p>Nothing Found</p>";
-    
-    }
-    }
-    
+        
 ?>
