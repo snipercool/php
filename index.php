59af11f552c8f0da8bd0f0996524ca8ea39a997e
snipercool <sniper.cool@hotmail.com>
2019-05-11 17:22:08 +0200

    starting sass to ux and ui

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cssgram-cssgram.netdna-ssl.com/cssgram.min.css">
-    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="dist/css/app.css">
     <title>Sockening</title>
 </head>
 <body>
